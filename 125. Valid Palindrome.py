b4d47e382e170057495003c44bd743c65cd056f4
Sharptooth <bolia@yeah.net>
2018-11-22 20:34:39 +0800

    no. 125

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -11,10 +11,25 @@
 # Input: "race a car"
 # Output: false
 
+def is_palindrome(word):
+    return word==word[::-1]
+
 class Solution:
     def isPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        
+        if s == None or s == '':
+            return True
+        # ss = ''
+        # for i in s:
+        #     if i.isalnum():
+        #         ss.join(i.lower())
+        ss = ''.join([x.lower() for x in s if x.isalnum()])
+        return ss==ss[::-1]
+
+
+
+
+print(Solution().isPalindrome('0P'))
